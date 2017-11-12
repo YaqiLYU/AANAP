a34b9dfa3152e07a94bfd52513deef813c68ec0a
Yaqi LYU <lvyaqinbu@163.com>
2017-11-12 20:03:41 +0800

    Update ransac_global_similarity.m

--- ransac_global_similarity.m
+++ ransac_global_similarity.m
@@ -12,7 +12,6 @@ hold on;
 pause(0.5)
 
 for i = 1:20
-    % Multi-GS内点估计
     [ ~,res,~,~ ] = multigsSampling(100,data,M,10);
     con = sum(res<=thr_l);
     [ ~, maxinx ] = max(con);
@@ -22,17 +21,14 @@ for i = 1:20
     end
     data_inliers = data(:,inliers);
 
-    % 提取参数计算S
     x  = data_inliers(1,:); 
     y  = data_inliers(2,:); 
     x_ = data_inliers(4,:);     
     y_ = data_inliers(5,:);
     
-    % 计算全局相似变换
     A = [];
     b = [];
     
-    % 相似变换：平移，旋转，尺度
     for idx = 1:size(x,2)
         A = [A; x(idx) -y(idx) 1 0;
                 y(idx)  x(idx) 0 1];
@@ -47,7 +43,6 @@ for i = 1:20
                          0        0       1];
     theta(i)     = atan(beta(2)/beta(1));
     
-    % 画内点分割结果
     clr = [rand(),0,rand()];
     plot(data_orig(1,inliers),data_orig(2,inliers),...
          'o','color',clr,'LineWidth',2);
@@ -56,13 +51,11 @@ for i = 1:20
     hold on;
     pause(0.5);
 
-    % 移除内点，更新循环变量
     outliers = find(res(:,maxinx)>thr_l);
     data = data(:,outliers);
     data_orig = data_orig(:,outliers);
 end
 
-% 旋转最小的S为最优
 index = find(abs(theta) == min(abs(theta)));
 S = S_segment{index};
 end
