fe13edd021863968d35fe95d9fdcc088ca177706
Yaqi LYU <lvyaqinbu@163.com>
2017-11-12 18:12:59 +0800

    Create apply_mdlt_transform.m

---
+++ apply_mdlt_transform.m
@@ -0,0 +1,23 @@
+function [out_x, out_y] = apply_mdlt_transform(x, y, Hmdlt)
+
+out_x = zeros(size(x));
+out_y = zeros(size(y));
+
+for i = 1:100
+    for j = 1:100
+        
+        grididx = (i-1) * size(y,2) + j;
+        T = reshape(Hmdlt(grididx,:),3,3);
+        T = inv(T);
+        
+        % 补偿网格平移变换
+        in_x = x(j,i);
+        in_y = y(j,i);
+        
+        out_x(j,i) = (T(1, 1)*in_x + T(1, 2)*in_y + T(1, 3)) ./ ...
+                     (T(3, 1)*in_x + T(3, 2)*in_y + T(3, 3));
+
+        out_y(j,i) = (T(2, 1)*in_x + T(2, 2)*in_y + T(2, 3)) ./ ...
+                     (T(3, 1)*in_x + T(3, 2)*in_y + T(3, 3));
+    end
+end
